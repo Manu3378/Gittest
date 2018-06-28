e5685a19f777ff22f71518f39094002385486108
Emmanuel ROBACHE <manu337810@gmail.com>
2018-06-28 11:37:11 +0200

    modif h1

--- index.php
+++ index.php
@@ -4,12 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Git test</title>
+    <style>
+    h1{
+
+        position:absolute;
+        top : 45%;
+        left : 45%;
+        color: blue;
+    
+    }
+    </style>
 </head>
 
 
 <body>
-    <h1>Vivement le mechoui ce midi!!!</h1>
+    <h1>Vivement France-Argentine Samedi!!!</h1>
 </body>
 
 
